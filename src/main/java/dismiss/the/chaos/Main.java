f0a4bd1625d3bdbcde8478cdfd8b7f90645499de
Kovács Tamás <tommy1991@vipmail.hu>
2015-10-14 10:55:11 +0200

    the application now does some printing

--- src/main/java/dismiss/the/chaos/Main.java
+++ src/main/java/dismiss/the/chaos/Main.java
@@ -3,7 +3,16 @@ import java.util.List;
 class Main {
 	
 	public static void main(String[] args) {
-		
+		System.out.println("Application starting...");
+		System.out.println("Calculating random number");
+		int rnd = (int)(Math.random() * 123);
+		System.out.println("The random number is: " + rnd);
+		System.out.println("Calling the line separator method");
+		insertSeparatorLine();
+	}//main
+	
+	static void insertSeparatorLine() {
+		System.out.println("---------------------------------");
 	}
 	
-}
+}//class
